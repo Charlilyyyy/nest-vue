db55eeddc19bb3bc898b8ec51afb91a6df82e949
taufikcharlie <mohamadtaufik099@gmail.com>
2024-10-31 21:34:59 +0800

    init vue

---
+++ vuejs/src/components/HelloWorld.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+    <UserList />
+  </div>
+</template>
+
+<script setup>
+import UserList from '../components/UserList.vue';
+</script>
+
+<style>
+/* Add global styles here if needed */
+</style>
---
+++ vuejs/src/components/UserList.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+      <h1>User List</h1>
+      <ul>
+        <li v-for="user in users" :key="user.id">{{ user.name }}</li>
+      </ul>
+      <button @click="fetchUsers">Fetch Users</button>
+    </div>
+  </template>
+  
+  <script>
+  import axios from 'axios';
+  
+  export default {
+    data() {
+      return {
+        users: [],
+      };
+    },
+    methods: {
+      async fetchUsers() {
+        try {
+          const response = await axios.get('http://localhost:3000/users');
+          this.users = response.data;
+          console.log(this.users)
+        } catch (error) {
+          console.error('Error fetching users:', error);
+        }
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  /* Add styles here if needed */
+  </style>
+  
