d401acf5e3eb299a1664b61b157495d4651b9e6e
taufikcharlie <mohamadtaufik099@gmail.com>
2024-11-02 20:34:34 +0800

    add mongoose

--- nestjs/src/app.module.ts
+++ nestjs/src/app.module.ts
@@ -1,11 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserController } from './users/users.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController,UserController],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Make ConfigModule globally available
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI'), // Get the MongoDB URI from the config
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  controllers: [AppController, UserController],
   providers: [AppService],
 })
 export class AppModule {}
